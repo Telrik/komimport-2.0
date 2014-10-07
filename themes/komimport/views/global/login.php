c2225f4852b88ddc0c9fd43ceab4b779b43511d3
Telrik <fs@m7.ru>
2014-10-07 20:12:54 +0800

    cl-commit

--- themes/komimport/views/global/login.php
+++ themes/komimport/views/global/login.php
@@ -1,8 +1,6 @@
-<H1>C:\OpenServer\domains\komimport.again\themes\komimport\views\global\</H1>
-
 <?php
-echo '<pre>' . print_r($_REQUEST, true) . '</pre>';
-
+//echo '<pre>' . print_r($_REQUEST, true) . '</pre>';
+//<H1>C:\OpenServer\domains\komimport.again\themes\komimport\views\global\</H1>
 $this->pageTitle = Yii::t('UserModule.user', 'Sign in');
 $this->breadcrumbs = array(Yii::t('UserModule.user', 'Sign in'));
 ?>
@@ -23,17 +21,17 @@ $this->breadcrumbs = array(Yii::t('UserModule.user', 'Sign in'));
 
 <?php echo $form->errorSummary($model); ?>
 
-<div class='row'>
-    <div class="col-xs-6">
-        <?php echo $form->textFieldGroup($model, 'email'); ?>
+    <div class='row'>
+        <div class="col-xs-6">
+            <?php echo $form->textFieldGroup($model, 'email'); ?>
+        </div>
     </div>
-</div>
 
-<div class='row'>
-    <div class="col-xs-6">
-        <?php echo $form->passwordFieldGroup($model, 'password'); ?>
+    <div class='row'>
+        <div class="col-xs-6">
+            <?php echo $form->passwordFieldGroup($model, 'password'); ?>
+        </div>
     </div>
-</div>
 
 <?php if ($this->getModule()->sessionLifeTime > 0): { ?>
     <div class='row'>
@@ -71,34 +69,34 @@ $this->breadcrumbs = array(Yii::t('UserModule.user', 'Sign in'));
 <?php } endif; ?>
 
 
-<div class="row">
-    <div class="col-xs-12">
-        <?php
-        $this->widget(
-            'bootstrap.widgets.TbButton',
-            array(
-                'buttonType' => 'submit',
-                'context' => 'primary',
-                'icon' => 'glyphicon glyphicon-signin',
-                'label' => Yii::t('UserModule.user', 'Sign in'),
-                'htmlOptions' => array('id' => 'login-btn', 'name' => 'login-btn')
-            )
-        ); ?>
+    <div class="row">
+        <div class="col-xs-12">
+            <?php
+            $this->widget(
+                'bootstrap.widgets.TbButton',
+                array(
+                    'buttonType' => 'submit',
+                    'context' => 'primary',
+                    'icon' => 'glyphicon glyphicon-signin',
+                    'label' => Yii::t('UserModule.user', 'Sign in'),
+                    'htmlOptions' => array('id' => 'login-btn', 'name' => 'login-btn')
+                )
+            ); ?>
 
-        <?php
-        $this->widget(
-            'bootstrap.widgets.TbButton',
-            array(
-                'buttonType' => 'link',
-                'context' => 'link',
-                'label' => Yii::t('UserModule.user', 'Sign up'),
-                'url' => Yii::app()->createUrl('/user/account/registration'),
-            )
-        ); ?>
+            <?php
+            $this->widget(
+                'bootstrap.widgets.TbButton',
+                array(
+                    'buttonType' => 'link',
+                    'context' => 'link',
+                    'label' => Yii::t('UserModule.user', 'Sign up'),
+                    'url' => Yii::app()->createUrl('/user/account/registration'),
+                )
+            ); ?>
+        </div>
     </div>
-</div>
 
-<hr/>
+    <hr/>
 
 <?php if (Yii::app()->hasModule('social')): { ?>
     <div class="row">
@@ -115,11 +113,11 @@ $this->breadcrumbs = array(Yii::t('UserModule.user', 'Sign in'));
         </div>
     </div>
 <?php } endif; ?>
-<div class="row">
-    <div class="col-xs-12">
-        <?php echo CHtml::link(Yii::t('UserModule.user', 'Forgot your password?'), array('/user/account/recovery')) ?>
+    <div class="row">
+        <div class="col-xs-12">
+            <?php echo CHtml::link(Yii::t('UserModule.user', 'Forgot your password?'), array('/user/account/recovery')) ?>
+        </div>
     </div>
-</div>
 
 <?php $this->endWidget(); ?>
 <?php
