613a305ad1dcfd013bdda374f54fa6f335c39a5c
Telrik <fs@m7.ru>
2014-10-09 00:44:14 +0800

    cl-commit

--- themes/komimport/views/menu/widgets/MenuWidget/main.php
+++ themes/komimport/views/menu/widgets/MenuWidget/main.php
@@ -1,8 +1,35 @@
 <?php
 Yii::import('application.modules.menu.components.YMenu');
 
-$auth_menu = Menu::model()->getItems('auth-menu', 0);
 
+$auth_menu = Menu::model()->getItems('auth-menu', 0);
+//print_r($auth_menu);
+array_unshift($auth_menu, array('label' => '<a style="color:#FFF;font-weight:bold;" href="/profile" class="listItemLink" title="Профиль">' . Yii::app()->user->profile->email . '</a>'));
+
+/*
+[label] => <i class="fa fa-unlock" title="Регистрация"></i><span class="hidden-sm hidden-xs">Регистрация</span>
+[template] => {menu}
+            [itemOptions] => Array
+(
+    [class] => listItem login-text pull-right
+                )
+
+            [linkOptions] => Array
+(
+    [class] => listItemLink
+                    [title] => Регистрация на сайте
+                )
+
+            [visible] => 1
+            [url] => Array
+(
+    [0] => /registration
+                )
+
+            [items] => Array
+(
+)
+*/
 $this->widget(
     'bootstrap.widgets.TbNavbar',
     array(
